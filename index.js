9d655fbd4c52a0b6b1f2976866bf4faddfd9cc58
Aziza  iliasova <miacorporation25@gmail.com>
2021-05-27 08:37:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+
+  let num1=62;
+  let num2=1;
+  let multiply=num1*num2;
+
+  let random = 0;
+  ++random;
+  random;
+
+let max = (1, 2, 3, 4, 5, 20);
+  Math.max;
+
+let num3=16;
+let num4=6;
+let mod= num3%num4;
